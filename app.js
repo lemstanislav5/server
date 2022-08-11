775cfcdfd5a1b6948639d424c3a6a3bd53cfa8cc
lemstanislav5 <lemstanislav5@gmail.com>
2022-08-12 00:02:58 +0300

    add mysql connect

--- app.js
+++ app.js
@@ -25,27 +25,30 @@ const server = https
 app.use('/.well-known/acme-challenge', verificationFile)
 app.use('/api', routes);
 
-console.log(host, user, password, database)
-// Create connection configuration
 
-const connection = mysql.createConnection({
-    host, user, password, database
+let connection = mysql.createConnection({host, user, password, database});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("Error occurred", err);
+  } else {
+    console.log("Success connection!");
+  }
 });
 
-// // Connect to the server
-connection.connect((err) => {
-    if (err) {
-      // Return error if present
-      console.log("Error occurred", err);
-    } else {
-      // Create database
-      console.log("Connected to MySQL Server");
-      const query = "CREATE DATABASE " + database;
-      connection.query(query, function (err, result) {
-        if (err) {
-          err;
-        }
-        console.log("New database created");
-      });
+const sql = `create table if not exists users(
+  id int primary key auto_increment,
+  login varchar(255) not null,
+  passwoed varchar(255) not null
+)`;
+
+connection.query(sql, function(err, res) {
+    if(err){
+      console.log(err);
+    } else if(res.warningCount === 0){
+      console.log("Таблица создана впервые");
+    } else if(res.warningCount === 1){
+      console.log("Таблица уже существует");
     }
 });
+connection.end();
